77903e2f6f67c9cac8fff6fe2d6743a127b5eb0c
Dean Armada <deanarmada@Deans-MacBook.local>
2017-05-25 07:45:51 +0800

    Quick fix

--- exam/__init__.py
+++ exam/__init__.py
@@ -1,18 +1,23 @@
+import os
 from datetime import timedelta
 from celery import Celery
 from celery.decorators import periodic_task
 from scrapy.crawler import CrawlerProcess
 from spiders.news import NewsSpider
 
+if os.environ.get('ENV') == 'cloud':
+    broker = 'amqp://guest:guest@rabbitmq:5672//'
+else:
+    broker = 'amqp://guest:guest@localhost:5672/'
 
-app = Celery(broker='amqp://guest:guest@localhost:5672/')
+app = Celery(broker=broker)
 
 process = CrawlerProcess({
     'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
 })
 
 
-@periodic_task(run_every=timedelta(minuites=10))
+@periodic_task(run_every=timedelta(minutes=10))
 def task_scrape_news():
     process.crawl(NewsSpider)
     process.start()
